f61da8eac7868ef976c82c7381381c0fbd9eacaf
Pratyush1606 <jaiswalpratyush2015@gmail.com>
2021-10-23 19:23:10 +0530

    Added code for Question 2 (A)

---
+++ Assignment2/gen_centrality.py
@@ -0,0 +1,108 @@
+from pathlib import Path
+from collections import defaultdict, deque
+import os
+
+from matplotlib.pyplot import viridis
+
+# Configuring file locations of datasets, plots and output files
+from config import CONFIG
+
+# Dataset
+DATASET = os.path.join(CONFIG["DATASET_DIR"], "imdb_prodco.adj")
+
+# Plot Directory
+PLOT_DIR = CONFIG["PLOT_DIR"]
+
+# Output Text File
+OUTPUT_FILE = Path(".\output_2_closeness.txt")
+
+# Making the blank output files
+with open(OUTPUT_FILE, "w") as f:
+    f.write("")
+
+###################################################
+
+def get_shortest_length_path_from_source(node, graph):
+    '''
+    Get the shortest path lengths to all the accessible nodes from the given node
+
+    Returns a list of shortest distances to accessible nodes from given node
+    '''
+    dist = []
+    visited = defaultdict(bool)
+    visited[node] = True
+    q = deque()
+    q.append((node, 0))
+    while(q):
+        curr_node, curr_dist = q.popleft()
+        for neigh, weight in graph[curr_node]:
+            if(not visited[neigh]):
+                visited[neigh] = True
+                neigh_dist = curr_dist + 1
+                # Have to confirm if here edge weight has to be considered or not 
+                # if considered then add weight instead of 1
+
+                q.append((neigh, neigh_dist))
+                dist.append(neigh_dist)
+    return dist
+
+def closeness_centrality(V, graph):
+    '''
+    Calculate closeness_centrality of a graph
+
+    Inputs:
+    V: Number of total nodes in the graph
+    graph: Adjacency List
+
+    Returns:
+    closeness_centrality: List of all the closeness_centrality of the nodes
+    '''
+    closeness_centrality = [0]*V
+    
+    for node in range(V):
+        shortest_path_lengths = get_shortest_length_path_from_source(node, graph)
+        tot_shortest_paths = sum(shortest_path_lengths)
+        len_shortest_paths = len(shortest_path_lengths)
+        node_closeness_centrality = 0
+        if(tot_shortest_paths>0 and V>1):
+            node_closeness_centrality = (len_shortest_paths - 1)/tot_shortest_paths
+        closeness_centrality[node] = round(node_closeness_centrality, 3)
+
+    return closeness_centrality
+
+# Reading Dataset
+with open(DATASET, "r") as file:
+    data = file.readlines()
+
+rows, columns = map(int, data[0].strip().split(","))
+
+# adjacency_mat = [[float("inf") for _ in range(columns)] for _ in range(rows)]
+# for line in data[1:]:
+#     u, v, w = map(int, line.strip().split(","))
+#     adjacency_mat[u][v] = w
+#     adjacency_mat[v][u] = w
+
+# # Making adjacency list
+# graph = defaultdict(list)
+# for i in range(rows):
+#     for j in range(columns):
+#         if(adjacency_mat[i][j]!=float("inf")):
+#             graph[i].append((j, adjacency_mat[i][j]))
+
+# Making adjacency list
+graph = defaultdict(list)
+for line in data[1:]:
+    u, v, w = map(int, line.strip().split(","))
+    graph[u].append((v, w))
+    graph[v].append((u, w))
+
+closeness_cenrality_list = closeness_centrality(rows, graph)
+
+# Writing output to the output file
+output = ""
+for node_ID, centrality_value in enumerate(closeness_cenrality_list):
+    output += "{} {}".format(node_ID, centrality_value)
+    output += "\n"
+
+with open(OUTPUT_FILE, "w") as f:
+    f.write(output)
--- Assignment2/q1.py
+++ Assignment2/q1.py
@@ -4,7 +4,6 @@ from pathlib import Path
 from networkx.algorithms import community
 from collections import defaultdict
 import os
-import sys
 
 # Configuring file locations of datasets, plots and output files
 from config import CONFIG
@@ -50,16 +49,6 @@ class Tee(object):
 
 # Configuring script to print to stdout as well as to different output files
 # backup = sys.stdout
-
-###################################################
-
-# Making undirected graph from DBLP Dataset
-Graphtype = nx.Graph()   # for undirected graph
-DBLP_GRAPH = nx.read_edgelist(DBLP_DATASET, create_using=Graphtype, nodetype=int)
-
-# Making directed graph from EUCORE Dataset
-Graphtype = nx.DiGraph()   # for directed graph
-EUCORE_GRAPH = nx.read_edgelist(EUCORE_DATASET, create_using=Graphtype, nodetype=int)
 
 ###################################################
 
@@ -117,9 +106,25 @@ def plot_ground_truth_community_size_distribution(methodname, datasetname, datas
     Plots the ground truth community size distribution for diff datasets and methods
     '''
     if(datasetname == DBLP_DATASET_NAME):
-        pass
+        '''
+        Dataset is in the form of
+        Com1_Node1 Com1_Node2 Com1_Node3 ....
+        .....       .....
+        '''
+        ground_truth_communities = []
+        # Reading the ground truth dataset
+        with open(dataset, "r") as f:
+            comm_list = f.readlines()
+        
+        for t in comm_list:
+            ground_truth_communities.append(list(map(int, t.strip().split())))
 
     elif(datasetname == EUCORE_DATASET_NAME):
+        '''
+        Dataset is in the form of
+        NodeId      Department
+        .....       .....
+        '''
         data = defaultdict(list)
 
         # Reading the ground truth dataset
@@ -129,18 +134,180 @@ def plot_ground_truth_community_size_distribution(methodname, datasetname, datas
         for t in node_id_dept_list:
             node_id, dept = map(int, t.strip().split())
             data[dept].append(node_id)
-        
-        x, y = [], []
-        for key, val in data.items():
-            x.append(key)
-            y.append(len(val))
+        ground_truth_communities = list(data.values())
+
+    # Calculating the frequency of the communities
+    freq = defaultdict(int)
+    for comm in ground_truth_communities:
+        freq[len(comm)] += 1
+
+    x, y = [], []
+    for key, val in freq.items():
+        x.append(key)
+        y.append(val)
         
     plt.plot(x, y)
     plt.xlabel("Size of Ground Truth Communities")
     plt.ylabel("Frequency")
-    plt.title("Ground Truth Community Size distribution of {} graph using {} method".format(datasetname, methodname))
+    plt.title("Ground Truth Community Size distribution of {} using {} method".format(datasetname, methodname))
     fig_destination = os.path.join(PLOT_DIR, f"ground_truth_communities_dist_{datasetname}_{methodname}.png")
     plt.savefig(fig_destination)
+    return ground_truth_communities
     
+###################################################
+
+def get_top_5_communities(communities):
+    '''
+    Get the top 5 communities based on the number of nodes from a list of communities'''
+    return sorted(communities, key=lambda com: len(com), reverse=True)[:5]
+
+###################################################
+
+def get_subragphs_from_communities(graph, communities):
+    '''
+    Get a list of subgraphs from a list of community nodes and the original graph
+    '''
+    communities_subgraphs = []
+    for com in communities:
+        subgraph = graph.subgraph(com).copy()
+        communities_subgraphs.append(subgraph)
+    
+    return communities_subgraphs
+
+###################################################
+
+def print_top_5_communities_size(methodname, datasetname, community_subgraphs):
+    '''
+    Print the size of the top 5 communities in the form of  (number of nodes, number of edges)
+    '''
+    print("Method: {}".format(methodname))
+    print("Dataset: {}".format(datasetname))
+    for i in range(len(community_subgraphs)):
+        community_subgraph = community_subgraphs[i]
+        number_of_nodes = community_subgraph.number_of_nodes()
+        number_of_edges = community_subgraph.number_of_edges()
+        print(f"Community {i+1}: (number_of_nodes = {number_of_nodes}, number_of_edges = {number_of_edges})")
+
+###################################################
+
+def get_community_coverage(methodname, datasetname, graph, communities):
+    '''
+    Get the coverage of the top 5 communities
+    '''
+    print("Method: {}".format(methodname))
+    print("Dataset: {}".format(datasetname))
+    for i in range(len(communities)):
+        com = communities[i]
+        coverage, performance = community.partition_quality(graph, [com])
+        print("Coverage of community {} = {}".format(i+1, coverage))
 
 
+def get_jaccard_coefficient(methodname, datasetname, graph1, graph2):
+    '''
+    https://networkx.org/documentation/stable/reference/algorithms/generated/networkx.algorithms.link_prediction.jaccard_coefficient.html#networkx.algorithms.link_prediction.jaccard_coefficient
+
+    https://stackoverflow.com/questions/50704439/python-jaccard-similartity-between-two-networks
+    '''
+    pass
+
+###################################################
+print()
+print("Question 1")
+print("-----------")
+print()
+
+# Making undirected graph from DBLP Dataset
+Graphtype = nx.Graph()   # for undirected graph
+DBLP_GRAPH = nx.read_edgelist(DBLP_DATASET, create_using=Graphtype, nodetype=int)
+
+# Making directed graph from EUCORE Dataset
+Graphtype = nx.DiGraph()   # for directed graph
+EUCORE_GRAPH = nx.read_edgelist(EUCORE_DATASET, create_using=Graphtype, nodetype=int)
+
+###################################################
+
+def run():
+    # Part A
+    print("[a]")
+
+    DBLP_NEWMAN_COMMUNITIES = get_num_of_communities(NEWMAN_METHOD_NAME, DBLP_DATASET_NAME, DBLP_GRAPH)
+    EUCORE_NEWMAN_COMMUNITIES = get_num_of_communities(NEWMAN_METHOD_NAME, EUCORE_DATASET_NAME, EUCORE_GRAPH)
+    DBLP_CLAUSET_COMMUNITIES = get_num_of_communities(CLAUSET_METHOD_NAME, DBLP_DATASET_NAME, DBLP_GRAPH)
+    EUCORE_CLAUSET_COMMUNITIES = get_num_of_communities(CLAUSET_METHOD_NAME, EUCORE_DATASET_NAME, EUCORE_GRAPH)
+
+    print()
+
+    ###################################################
+
+    # Part B
+    print("[b]")
+
+    plot_community_size_distribution(NEWMAN_METHOD_NAME, DBLP_DATASET_NAME, DBLP_NEWMAN_COMMUNITIES)
+    plot_community_size_distribution(NEWMAN_METHOD_NAME, EUCORE_DATASET_NAME, EUCORE_NEWMAN_COMMUNITIES)
+    plot_community_size_distribution(CLAUSET_METHOD_NAME, DBLP_DATASET_NAME, DBLP_CLAUSET_COMMUNITIES)
+    plot_community_size_distribution(CLAUSET_METHOD_NAME, EUCORE_DATASET_NAME, EUCORE_CLAUSET_COMMUNITIES)
+
+    print()
+
+    ###################################################
+
+    # Part C
+    print("[c]")
+
+    DBLP_NEWMAN_GROUND_TRUTH_COMMUNITIES = plot_ground_truth_community_size_distribution(NEWMAN_METHOD_NAME, DBLP_DATASET_NAME, DBLP_GROUND_TRUTH_COM_DATASET)
+    EUCORE_NEWMAN_GROUND_TRUTH_COMMUNITIES = plot_ground_truth_community_size_distribution(NEWMAN_METHOD_NAME, EUCORE_DATASET_NAME, EUCORE_GROUND_TRUTH_COM_DATASET)
+    DBLP_CLAUSET_GROUND_TRUTH_COMMUNITIES = plot_ground_truth_community_size_distribution(CLAUSET_METHOD_NAME, DBLP_DATASET_NAME, DBLP_GROUND_TRUTH_COM_DATASET)
+    EUCORE_CLAUSET_GROUND_TRUTH_COMMUNITIES = plot_ground_truth_community_size_distribution(CLAUSET_METHOD_NAME, EUCORE_DATASET_NAME, EUCORE_GROUND_TRUTH_COM_DATASET)
+
+    print()
+
+    ###################################################
+
+    # Part D
+    print("[d]")
+
+    DBLP_NEWMAN_TOP_5_COMMUNITIES = get_top_5_communities(DBLP_NEWMAN_COMMUNITIES)
+    EUCORE_NEWMAN_TOP_5_COMMUNITIES = get_top_5_communities(EUCORE_NEWMAN_COMMUNITIES)
+    DBLP_CLAUSET_TOP_5_COMMUNITIES =  get_top_5_communities(DBLP_CLAUSET_COMMUNITIES)
+    EUCORE_CLAUSET_TOP_5_COMMUNITIES = get_top_5_communities(EUCORE_CLAUSET_COMMUNITIES)
+
+    DBLP_NEWMAN_TOP_5_COMMUNITIES_SUBGRAPHS = get_subragphs_from_communities(DBLP_GRAPH, DBLP_NEWMAN_TOP_5_COMMUNITIES)
+    EUCORE_NEWMAN_TOP_5_COMMUNITIES_SUBGRAPHS = get_subragphs_from_communities(EUCORE_GRAPH, EUCORE_NEWMAN_TOP_5_COMMUNITIES)
+    DBLP_CLAUSET_TOP_5_COMMUNITIES_SUBGRAPHS =  get_subragphs_from_communities(DBLP_GRAPH, DBLP_CLAUSET_TOP_5_COMMUNITIES)
+    EUCORE_CLAUSET_TOP_5_COMMUNITIES_SUBGRAPHS = get_subragphs_from_communities(EUCORE_GRAPH, EUCORE_CLAUSET_TOP_5_COMMUNITIES)
+
+
+    print_top_5_communities_size(NEWMAN_METHOD_NAME, DBLP_DATASET_NAME, DBLP_NEWMAN_TOP_5_COMMUNITIES_SUBGRAPHS)
+    print_top_5_communities_size(NEWMAN_METHOD_NAME, EUCORE_DATASET_NAME, EUCORE_NEWMAN_TOP_5_COMMUNITIES_SUBGRAPHS)
+    print_top_5_communities_size(CLAUSET_METHOD_NAME, DBLP_DATASET_NAME, DBLP_CLAUSET_TOP_5_COMMUNITIES_SUBGRAPHS)
+    print_top_5_communities_size(CLAUSET_METHOD_NAME, EUCORE_DATASET_NAME, EUCORE_CLAUSET_TOP_5_COMMUNITIES_SUBGRAPHS)
+
+    print()
+
+    ###################################################
+
+    # Part E
+    print("[e]")
+
+    get_community_coverage(NEWMAN_METHOD_NAME, DBLP_DATASET_NAME, DBLP_GRAPH, DBLP_NEWMAN_TOP_5_COMMUNITIES)
+    get_community_coverage(NEWMAN_METHOD_NAME, EUCORE_DATASET_NAME, EUCORE_GRAPH, EUCORE_NEWMAN_TOP_5_COMMUNITIES)
+    get_community_coverage(CLAUSET_METHOD_NAME, DBLP_DATASET_NAME, DBLP_GRAPH, DBLP_CLAUSET_TOP_5_COMMUNITIES)
+    get_community_coverage(CLAUSET_METHOD_NAME, EUCORE_DATASET_NAME, EUCORE_GRAPH, EUCORE_CLAUSET_TOP_5_COMMUNITIES)
+
+    print()
+
+    ###################################################
+
+    # Part F
+    print("[f]")
+
+
+
+
+    print()
+
+
+def check():
+    DBLP_NEWMAN_GROUND_TRUTH_COMMUNITIES = plot_ground_truth_community_size_distribution(NEWMAN_METHOD_NAME, DBLP_DATASET_NAME, DBLP_GROUND_TRUTH_COM_DATASET)
+
+check()
